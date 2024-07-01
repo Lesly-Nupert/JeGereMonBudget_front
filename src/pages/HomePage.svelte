4947d47ebe234c665a0eed15a063c1465057d0e6
Lesly-Nupert <lesly.nupert@gmail.com>
2024-07-01 21:16:16 +0200

    feature/HomePage DONE

--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -1,15 +1,35 @@
 <script>
-
 </script>
 
+<main class="text-white text-center">
+    <i class="icon__euro bi bi-currency-euro text-warning"></i>
 
-<h1>Test</h1>
+    <h1 class="fs-1 text-primary m-5">Bienvenue dans MonBudget !</h1>
 
-<i class="icon__euro bi bi-currency-euro"></i>
+    <p class="fs-4">
+        MonBudget - Une application pour suivre facilement ses revenus et ses
+        dépenses
+    </p>
+
+    <a href="#" class="btn btn-warning m-5 fs-4">Connexion</a>
+
+    <p class="fs-4">
+        Vous n'avez pas encore de compte ? <br /><a href="#" class="text-primary">Inscrivez-vous</a>
+    </p>
+</main>
 
 <style>
+    main {
+        max-width: 1200px;
+        margin: 0 auto;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 10px;
+    }
     .icon__euro {
-        font-size: 2rem;
-        color: black;
+        font-size: 100px;
+        color: white;
     }
 </style>
