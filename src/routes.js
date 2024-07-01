17b62e4349caaddad6aa6e7b1cb3fd3e82a588e8
Lesly-Nupert <lesly.nupert@gmail.com>
2024-07-01 16:24:16 +0200

    Initial project setup

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+// import HomePage from "./pages/Homepage.svelte";
+// import Login from "./pages/Login.svelte";
+// import Signup from "./pages/Signup.svelte";
+// import AddAccount from "./pages/AddAccount.svelte";
+// import AddExpense from "./pages/AddExpense.svelte";
+// import AddIncome from "./pages/AddIncome.svelte";
+// import AccountWithTransactions from "./pages/AccountWithTransactions.svelte";
+// import Expense from "./pages/Expense.svelte";
+// import Income from "./pages/Income.svelte";
+// import UpdatedExpense from "./pages/UpdatedExpense.svelte";
+// import UpdatedIncome from "./pages/UpdatedIncome.svelte";
+// import UserAccount from "./pages/UserAccount.svelte";
+// import ListAccountNameByUser from "./pages/ListAccountNameByUser.svelte";
+// import UpdatedAccount from "./pages/UpdtedAccount.svelte";
+// import UpdatedPassword from "./pages/UpdatedPassword.svelte";
+// import ContactUs from "./pages/ContactUs.svelte";
+// import LegalNotice from "./pages/LegalNotice.svelte";
+// import NotFound from "./pages/NotFound.svelte";
+
+// export default {
+//     "/": HomePage,
+//     "/login": Login,
+//     "/signup": Signup,
+//     "/addAccount": AddAccount,
+//     "/addExpense": AddExpense,
+//     "/addIncome": AddIncome,
+//     "/accountWithTransactions/:id": AccountWithTransactions,
+//     "/expense/:id": Expense,
+//     "/income/:id": Income,
+//     "/updatedExpense/:id": UpdatedExpense,
+//     "/updatedIncome/:id": UpdatedIncome,
+//     "/userAccount/:userId": UserAccount,
+//     "/listAccountNameByUser/:userId": ListAccountNameByUser,
+//     "/updatedAccount/:userId": UpdatedAccount,
+//     "/updatedPassword/:userId": UpdatedPassword,
+//     "/contactUs ": ContactUs,
+//     "/legalNotice": LegalNotice,
+//     "*": NotFound,
+// };
